d72456a1711e1e7a4337ce1753a9d067d957c4c1
compiller25 <mwakasalasteven19@gmail.com>
2026-03-12 03:21:11 +0300

    Set router basename for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -23,7 +23,7 @@ const App = () => (
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
             <div className="flex min-h-screen flex-col">
               <div className="flex-1">
                 <Routes>
